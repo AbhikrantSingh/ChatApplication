80fb863ac9c50660a85056598f4900d21988cf3b
AbhikrantSingh <abhikrantsingh@gmail.com>
2023-06-10 02:15:16 +0530

    Started Learning abut SignalR

---
+++ NewChatApp/Hubs/UserHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace NewChatApp.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public static int TotalUser { get; set; } = 0;
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUser++;
+            Clients.All.SendAsync("updateTotalUser", TotalUser).GetAwaiter().GetResult();
+
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUser--;
+            Clients.All.SendAsync("updateTotalUser", TotalUser).GetAwaiter().GetResult();
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            //send update 
+            await Clients.All.SendAsync("updateTotalViews",TotalViews);
+        }
+    }
+}
---
+++ NewChatApp/wwwroot/js/usersCount.js
@@ -0,0 +1,34 @@
+﻿//create connection
+
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methodd that hub
+connectionUserCount.on("updateTotalViews", (value) => {
+
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+
+connectionUserCount.on("updateTotalUser", (value) => {
+
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+//start the connection
+
+function fulfilled() {
+    console.log("Success")
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    console.log("FAILED");
+}
+
+
+connectionUserCount.start().then(fulfilled, rejected)
